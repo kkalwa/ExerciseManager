d17d943682f895b28d3a4fbc03c9118ae5a3289a
kkalwa <kkalwa93@vp.pl>
2026-02-10 19:42:53 +0100

    Added Database Scheme object

---
+++ ExerciseManager/Schemes/MuscleDBScheme.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym
+{
+    internal static class MuscleDBScheme
+    {
+        
+        public static readonly string DB_NAME = "muscleDB";
+        public static readonly string STANDARD_CONNECTION_STRING = $"Server=localhost\\SQLEXPRESS;Database={DB_NAME};Trusted_Connection=yes;TrustServerCertificate=True";
+        private static readonly string prefix = "dbo.";
+        
+        
+        public static class UsersTable
+        {
+            private static string tableName = "Users";
+            public static string TableName => prefix + tableName;
+
+            public static class UsersColumns
+            {
+                public static readonly string IdUser = "Id_User";
+                public static readonly string Nickname = "Nickname";
+                public static readonly string Password = "Password";
+            }
+        }
+    }
+}
