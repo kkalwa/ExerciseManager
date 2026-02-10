c0c837aaa17bd0b8bd37ded59a4a257c31474172
kkalwa <kkalwa93@vp.pl>
2026-02-10 20:28:00 +0100

    End of day commit

--- ExerciseManager/ViewModels/LoginViewModel.cs
+++ ExerciseManager/ViewModels/LoginViewModel.cs
@@ -3,19 +3,60 @@ using ExerciseManager.Mediators;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace ExerciseManager.ViewModels
 {
     
     public class LoginViewModel: BaseViewModel
     {
-        public RelayCommand GoToUserPanelCommand { get; set; }
+        /** Private fields associated with properties
+         * 
+         **/
+        private string? login;
+        private string? loginStatus;
+        private bool startAnimation;
+
+        /** Public properties used by views
+         * 
+         **/
+        public string Login
+        {
+            get { return login ?? string.Empty; }
+            set { login = value; }
+        }
+        public string LoginStatus
+        {
+            get { return loginStatus ?? String.Empty; }
+            set
+            {
+                loginStatus = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool StartAnimation
+        {
+            get { return startAnimation; }
+            set
+            {
+                startAnimation = value;
+                OnPropertyChanged();
+            }
+        }
+        public string TitleText { get; } = "Zaloguj się";
+        public RelayCommand GoToUserPanelCommand { get; set; } //**************************************************** Remove later *****************************
+        public ICommand InitiateLogInProcessCommand { get; set; }
 
         public LoginViewModel(ViewMediator viewMediator): base(viewMediator)
         {
-            GoToUserPanelCommand = new RelayCommand(GoToUserPanel);
+            
         }
 
+
+
+        /** Stuff that probably will be removed in the future
+         * 
+         **/
         private void GoToUserPanel(object obj)
         {
             viewMediator.ChangeViewTo(new UserPanelViewModel(viewMediator));
